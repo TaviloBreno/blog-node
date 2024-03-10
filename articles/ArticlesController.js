282460ed5c84e3f02f54b8f629657b93fbd736e9
Tavilo Breno <breno_wk2@hotmail.com>
2024-03-09 21:42:06 -0300

    Salvando artigos

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -1,15 +1,33 @@
 const express = require("express");
 const router = express.Router();
-const Category = require('./../categories/Category');
+const Category = require("./../categories/Category");
+const Article = require("./Article");
+const slugify = require("slugify");
 
 router.get("/articles", (req, res) => {
   res.send("Rota de Artigos");
 });
 
 router.get("/admin/articles/new", (req, res) => {
-  Category.findAll().then(categories => {
-    res.render("admin/articles/new", {categories: categories});
-  })
+  Category.findAll().then((categories) => {
+    res.render("admin/articles/new", { categories: categories });
+  });
+});
+
+router.post("/articles/save", (req, res) => {
+  var title = req.body.title;
+  var body = req.body.body;
+  var category = req.body.category;
+
+  Article.create({
+    title: title,
+    slug: slugify(title, {
+      lower: true,
+      remove: /[*+~.()'"!:@]/g,
+    }),
+    body: body,
+    categoryId: category
+  });
 });
 
 module.exports = router;
