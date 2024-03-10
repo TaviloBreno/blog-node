a842993b7a7cdd86140e1316a93bb0465a7e005b
Tavilo Breno <breno_wk2@hotmail.com>
2024-03-10 01:46:37 -0300

    Finalizando o Projeto

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -5,7 +5,7 @@ const Article = require("./Article");
 const slugify = require("slugify");
 const adminAuth = require('./../middlewares/adminAuth');
 
-router.get("/admin/articles", (req, res) => {
+router.get("/admin/articles", adminAuth, (req, res) => {
   Article.findAll({
     include: [{ model: Category }],
   }).then((articles) => {
@@ -13,13 +13,13 @@ router.get("/admin/articles", (req, res) => {
   });
 });
 
-router.get("/admin/articles/new", (req, res) => {
+router.get("/admin/articles/new", adminAuth, (req, res) => {
   Category.findAll().then((categories) => {
     res.render("admin/articles/new", { categories: categories });
   });
 });
 
-router.post("/articles/save", (req, res) => {
+router.post("/articles/save", adminAuth, (req, res) => {
   var title = req.body.title;
   var body = req.body.body;
   var category = req.body.category;
@@ -37,7 +37,7 @@ router.post("/articles/save", (req, res) => {
   });
 });
 
-router.post("/articles/delete", (req, res) => {
+router.post("/articles/delete", adminAuth, (req, res) => {
   var id = req.body.id;
 
   if (id != undefined) {
@@ -57,7 +57,7 @@ router.post("/articles/delete", (req, res) => {
   }
 });
 
-router.get("/admin/articles/edit/:id", (req, res) => {
+router.get("/admin/articles/edit/:id", adminAuth, (req, res) => {
   var id = req.params.id;
   Article.findByPk(id)
     .then((article) => {
